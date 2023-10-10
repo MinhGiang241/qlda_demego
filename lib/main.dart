a5e75cac4b9371c37a6bd6441bc83e9a63b04ba2
Minh Giang <minhgiang241@gmail.com>
2023-10-10 10:01:26 +0700

    fix scalefactor error

--- lib/main.dart
+++ lib/main.dart
@@ -50,10 +50,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         builder: (context, child) {
           final mediaQueryData = MediaQuery.of(context);
-          final scale = mediaQueryData.textScaler
-              .clamp(minScaleFactor: 1.0, maxScaleFactor: 1.2);
+          final scale = mediaQueryData.textScaleFactor.clamp(1.0, 1.2);
           return MediaQuery(
-            data: MediaQuery.of(context).copyWith(textScaler: scale),
+            data: MediaQuery.of(context).copyWith(textScaleFactor: scale),
             child: child!,
           );
         },
